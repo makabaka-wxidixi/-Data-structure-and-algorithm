08b2f2e7c42b96b1d88595f855c3e81d28949e70
makabaka-wxidixi <85681650+makabaka-wxidixi@users.noreply.github.com>
2021-10-07 13:13:50 +0800

    Add files via upload

--- KrusKalDemo.java
+++ KrusKalDemo.java
@@ -19,12 +19,9 @@ public class KrusKalDemo {
                 {16, 7, 6, MAX, 2, 0, 9},
                 {14, MAX, MAX, MAX, 8, 9, 0}};
         Group group = new Group(data, weight);
-        group.printGroup();
-        Group.Edge[] edges = group.getEdges();
-        System.out.println(Arrays.toString(edges));
-        group.sortEdges(edges);
-        System.out.println(Arrays.toString(edges));
+        group.kruskal();
     }
+
 }
 
 class Group {
@@ -70,8 +67,8 @@ class Group {
         }
     }
 
-    //返回目标结点的下标
-    public int getVertexPosition(char target) {
+    //返回目标结点在结点数组中的下标
+    public int getPositionInVertexArr(char target) {
         for (int i = 0; i < this.numOfVertex; i++) {
             if (this.vertex[i] == target) {
                 return i;
@@ -116,11 +113,56 @@ class Group {
         return edges;
     }
 
+    /**
+     * 功能：获取下标为target的顶点的终点，用于后序判断两个顶点的终点是否相同
+     *
+     * @param ends   记录了各个顶点的对应的重点，再遍历中得到
+     * @param target 目标顶点的下标
+     * @return 返回目标顶点的终点的下标
+     */
     public int getEnd(int[] ends, int target) {
+        /*
+        算法核心
+        举例：A->B->D->C->E->G，假设最后的最小生成树就是这样的
+        单看A->B，A的终点似乎就是B，但是不要忘了B->D，那么A的终点就是D？，也不对，因为D后面害连着一个C...
+        该循环就可以一直寻找，直至找到0，也就是最后的G才会停止。此时返回的就是G的下标
+         */
         while (ends[target] != 0) {
             target = ends[target];
         }
         return target;
+    }
+
+    /**
+     * 思路：
+     * 将图的每条边进行从小到大排列，确保先连接最小的边
+     * 然后依次遍历每条边
+     * 如果这条边的两个顶点的终点相等，那么就证明是循环的，不能连接
+     * 反之，如果这两条边的终点不等，那么就可以进行连接。
+     */
+    public void kruskal() {
+        int index = 0;//指向结果数组
+        int[] endsVertex = new int[numOfVertex];//用于记录每个顶点的终点坐标
+        Edge[] rets = new Edge[numOfVertex-1];//用于存储最小生成树
+        Edge[] edges = this.getEdges();//得到图中的所有边
+        this.sortEdges(edges);//将得到的边数组从小到大排序
+        for (int i = 0; i < numOfEdges; i++) {
+            //得到目标边的两个顶点
+            char startVertex = edges[i].start;
+            char endVertex = edges[i].end;
+            //得到两个顶点在顶点数组中的下标
+            int startPositionInVertexArr = this.getPositionInVertexArr(startVertex);
+            int endPositionInVertexArr = this.getPositionInVertexArr(endVertex);
+            //得到两个顶点的终点结点的下标
+            int startEnd = this.getEnd(endsVertex, startPositionInVertexArr);
+            int endEnd = this.getEnd(endsVertex, endPositionInVertexArr);
+            //如果这两个顶点的终点不同，那么就可以连接
+            if (startEnd != endEnd) {
+                endsVertex[startEnd] = endEnd;
+                rets[index++] = edges[i];
+            }
+        }
+        System.out.println(Arrays.toString(rets));
     }
 
     class Edge {
