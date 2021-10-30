e3e6325fa80edf3ceccdcfb46c798eb8c4e3bb4f
makabaka-wxidixi <85681650+makabaka-wxidixi@users.noreply.github.com>
2021-10-30 09:42:27 +0800

    Add files via upload

--- Linked_list/src/single/DeleteNode.java
+++ Linked_list/src/single/DeleteNode.java
@@ -1,5 +1,7 @@
 package single;
 
+import java.util.Stack;
+
 /**
  * @Author mkbk
  * @Description
@@ -13,8 +15,16 @@ public class DeleteNode {
         public int val;
         public Node next;
 
+        public Node() {
+        }
+
         public Node(int val) {
             this.val = val;
+        }
+
+        public Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
         }
 
         @Override
@@ -46,7 +56,7 @@ public class DeleteNode {
         tail = node;
     }
 
-    public void list() {
+    public void list(Node head) {
         if (head == null) {
             System.out.println("链表为空");
             return;
@@ -63,30 +73,34 @@ public class DeleteNode {
         for (int i = 0; i < 10; i++) {
             list.insertByHead(i);
         }
-        list.list();
+        Node node = list.removeNthFromEnd4(list.head, 10);
+        list.list(node);
     }
 
-    //***************************************以下是主体函数*******************************************
+    //**************************************删除倒数第N个结点*******************************************
 
     /**
-     * 删除倒数第N个结点
+     * 方式一：迭代。先遍历链表，得到长度。然后for循环找前驱结点
+     * 时间复杂度：On     <n,2n>
+     * 空间复杂度：O1
+     *
      * @param head 链表头结点
-     * @param n 倒数第n个结点
+     * @param n    倒数第n个结点
      * @return 返回删除之后的链表头结点
      */
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(0, head);
+    public Node removeNthFromEnd(Node head, int n) {
+        Node dummy = new Node();
+        dummy.next = head;
         int length = getLength(head);
-        ListNode cur = dummy;
+        Node cur = dummy;
         for (int i = 1; i < length - n + 1; ++i) {
             cur = cur.next;
         }
         cur.next = cur.next.next;
-        ListNode ans = dummy.next;
-        return ans;
+        return dummy.next;
     }
 
-    public int getLength(ListNode head) {
+    public int getLength(Node head) {
         int length = 0;
         while (head != null) {
             ++length;
@@ -95,4 +109,79 @@ public class DeleteNode {
         return length;
     }
 
+    /**
+     * 方式二：栈。根据栈的先入后出的特性来找到要删除结点的前驱结点
+     * 时间复杂度：On
+     * 空间复杂度：On（主要是栈的开销）
+     *
+     * @param head 链表头结点
+     * @param n    倒数第n个结点
+     * @return 返回删除之后的链表头结点
+     */
+    public Node removeNthFromEnd2(Node head, int n) {
+        Node hair = new Node();
+        hair.next = head;
+        Node curr = hair;
+        Stack<Node> stack = new Stack<>();
+        while (curr != null) {
+            stack.push(curr);
+            curr = curr.next;
+        }//入栈
+        for (int i = 0; i < n; i++) {
+            stack.pop();
+        }
+        Node prev = stack.peek();//找到要删除结点的前驱结点
+        prev.next = prev.next.next;
+        return hair.next;
+    }
+
+    /**
+     * 方式三：左右指针，设置一个偏移量，右指针到达右边界的时候，左指针就指向了要删除结点的前驱结点
+     *
+     * @param head 链表头结点
+     * @param n    倒数第n个结点（偏移量）
+     * @return 返回删除之后的链表头结点
+     */
+    public Node removeNthFromEnd3(Node head, int n) {
+        Node hair = new Node();
+        hair.next = head;
+        Node left = hair, right = left;//左右指针
+        for (int i = 0; i < n; i++) {
+            right = right.next;
+        }
+        while (right.next != null) {//当右指针碰到右边界时，此时左指针指向的就是要删除结点的前驱结点
+            right = right.next;
+            left = left.next;
+        }
+        left.next = left.next.next;//left是前驱结点
+        return hair.next;
+    }
+
+    /**
+     * 方式四：递归。回溯过程中用一个变量和n作比较，来寻找目标结点
+     * 时间复杂度：On
+     * 空间复杂度：On（堆栈帧）
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    public int j;//用于回溯时寻找要删除的节点
+
+    public Node removeNthFromEnd4(Node head, int n) {
+        Node hair = new Node(0, head);
+        recursion(hair, n);
+        return hair.next;
+    }
+
+    private void recursion(Node head, int n) {
+        if (head.next == null) {//最后一个结点就返回
+            return;
+        }
+        recursion(head.next, n);
+        j++;
+        if (j == n) {
+            head.next = head.next.next;//head就是倒数第j个结点的前驱结点
+        }
+    }
 }
