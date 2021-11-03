73af478035a5ad3539ee6050f8984cae048a5929
makabaka-wxidixi <85681650+makabaka-wxidixi@users.noreply.github.com>
2021-11-03 22:09:36 +0800

    Add files via upload

--- Linked_list/src/single/SwapNode.java
+++ Linked_list/src/single/SwapNode.java
@@ -77,11 +77,11 @@ public class SwapNode {
             list.insertByTail(i);
         }
         list.list(list.head);
-        Node node = list.swapNodes2(list.head);
+        Node node = list.swapNode(list.head,5);
         list.list(node);
     }
 
-    //*******************************************交换相邻结点*******************************************
+    //*******************************************交换相邻奇偶结点*******************************************
 
     /**
      * 方式一：迭代
@@ -111,7 +111,7 @@ public class SwapNode {
     }
 
     /**
-     * 方式一：递归
+     * 方式二：递归
      * 时间复杂度：On
      * 空间复杂度：On（栈帧）
      *
@@ -126,5 +126,49 @@ public class SwapNode {
         head.next = swapNodes2(temp.next);
         temp.next = head;
         return temp;
+    }
+
+    //*****************************************交换链表对称结点*****************************************
+
+    /**
+     * 方式一：双指针（一次遍历）
+     * 时间复杂度：On（整个过程fast从头到尾遍历一遍，之后就是判断）
+     * 空间复杂度：O1
+     *
+     * @param head 链表头结点
+     * @return 返回修改后的链表
+     */
+    public Node swapNode(Node head, int k) {
+        if (head == null) {
+            return null;
+        }
+        Node hair = new Node(-1, head);//创建哑结点，因为头结点可能会被交换
+        Node slow = hair, fast = hair;//前后指针，定位两个结点，
+        for (int i = 1; i < k; i++) {
+            fast = fast.next;
+        }
+        Node leftPrev = fast, left = fast.next, leftRear = fast.next.next;//左节点前驱，左节点，左节点后继节点
+        while (fast.next.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        Node rightPrev = slow, right = slow.next, rightRear = slow.next.next;//右结点前驱，右结点，右结点后继节点
+        if (leftRear == right) {//左节点是右结点的前驱
+            leftPrev.next = right;
+            right.next = left;
+            left.next = rightRear;
+        } else if (leftPrev == right) {//左节点是右结点的后继
+            rightPrev.next = left;
+            left.next = right;
+            right.next = leftRear;
+        } else {//普通情况
+            //左节点的前驱指向右结点，右结点的后继指向原左节点的后继
+            leftPrev.next = right;
+            right.next = leftRear;
+            //右结点的前驱指向左节点，左节点的后继指向原右结点的后继
+            rightPrev.next = left;
+            left.next = rightRear;
+        }
+        return hair.next;
     }
 }
